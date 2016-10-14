d30c1ad7675765647cb74f857a9b77cd7edfd51c
Gwendal Roué <gr@pierlis.com>
2016-10-14 08:09:22 +0200

    Initial Commit

---
+++ WWDCCompanion/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WWDCCompanion
+//
+//  Created by Gwendal Roué on 14/10/2016.
+//  Copyright © 2016 Gwendal Roué. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
